d73b79cd2283af2ba894bfa72888b365d3daee63
iv <sergeyivanychev@gmail.com>
2018-05-16 01:24:22 +0300

    Implemented joint mapping and brought some examples

---
+++ Ivanychev2018/code/lapprox/joint/joint_mapping.py
@@ -0,0 +1,31 @@
+from typing import Dict
+
+import numpy as np
+import sklearn.base
+
+from ..models import base
+
+class JointMapping(sklearn.base.TransformerMixin,
+                   sklearn.base.BaseEstimator):
+    def __init__(self, models: Dict[str, base.BaseLocalModel]):
+        self._models = models
+
+
+    def fit(self, X, y=None) -> 'JointMapping':
+        for name, model in self._models.items():
+            model.fit(X)
+        return self
+
+    def transform(self, X=None) -> np.ndarray:
+        self._transformed = {name: model.transform()
+                             for name, model in self._models.items()}
+        last_index = 0
+        for name, transformed in self._transformed.items():
+            next_index = last_index + transformed.shape[1]
+            print(f"Model {name}: indices {last_index}–{next_index}")
+            last_index = next_index
+        return np.concatenate(list(self._transformed.values()), axis=1)
+
+    @property
+    def models(self):
+        return self._models
--- Ivanychev2018/code/lapprox/models/fft.py
+++ Ivanychev2018/code/lapprox/models/fft.py
@@ -4,6 +4,9 @@ import scipy.fftpack as fftpack
 from . import base
 
 class Fft(base.BaseLocalModel):
+    def __init__(self) -> None:
+        pass
+    
     def fit_row(self, row: np.ndarray) -> base.ApproxAndParams:
         params = fftpack.fft(row)
         predicted = fftpack.ifft(params)
