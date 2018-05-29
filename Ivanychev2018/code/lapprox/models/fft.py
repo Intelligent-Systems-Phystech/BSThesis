86385071d76b365a11cf87056e9c5b5986db6920
iv <sergeyivanychev@gmail.com>
2018-05-29 20:21:13 +0300

    Fixed FFT so it now returns top harmonics instead of FFT

--- Ivanychev2018/code/lapprox/models/fft.py
+++ Ivanychev2018/code/lapprox/models/fft.py
@@ -8,11 +8,12 @@ import scipy.fftpack as fftpack
 from . import base
 
 class Fft(base.BaseLocalModel):
-    def __init__(self) -> None:
+    def __init__(self, n_harmonics: int) -> None:
+        self._n_harmonics = n_harmonics
         pass
     
     def fit_row(self, row: np.ndarray) -> base.ApproxAndParams:
         params = fftpack.fft(row)
+        top_harmonics_idx = np.argsort(np.abs(params))[-self._n_harmonics:]
         predicted = fftpack.ifft(params)
-        return base.ApproxAndParams(predicted, params)
-
+        return base.ApproxAndParams(predicted, top_harmonics_idx[::-1])
